6b461cacd7d1c23693733832a5555645dcfafa9c
Juliano Damasio <julianodamasiosbp@gmail.com>
2021-10-15 22:26:42 -0300

    Criando o primeiro endpoint para criação de uma tarefa

--- src/main/java/com/acmecompany/todolist/controller/TaskController.java
+++ src/main/java/com/acmecompany/todolist/controller/TaskController.java
@@ -1,4 +1,21 @@
 package com.acmecompany.todolist.controller;
 
+import com.acmecompany.todolist.model.Task;
+import com.acmecompany.todolist.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
 public class TaskController {
+
+    @Autowired
+    TaskService taskService;
+
+    @PostMapping("/tasks")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task createTask(@RequestBody Task task){
+        return taskService.createTask(task);
+    }
 }
--- src/main/java/com/acmecompany/todolist/service/TaskService.java
+++ src/main/java/com/acmecompany/todolist/service/TaskService.java
@@ -1,4 +1,19 @@
 package com.acmecompany.todolist.service;
 
+import com.acmecompany.todolist.model.Task;
+import com.acmecompany.todolist.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createTask(Task task){
+        return taskRepository.save(task);
+    }
 }
